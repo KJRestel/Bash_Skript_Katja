053dbf272cd5c97e941a34d3006744f4c691b52b
Katja Restel <restel@ub.uni-koeln.de>
2023-02-06 14:23:47 +0100

    Initial commit

---
+++ mein_bash_skript.sh
@@ -0,0 +1,10 @@
+# Mit diesem Skript begrüße ich euch alle
+# Kommentare füge ich hinter ein hash Zeichen 
+
+echo "hallo zusammen"
+for FILE in *.txt
+do
+echo $FILE
+head -n 2 $FILE
+tail -n 2 $FILE
+done
